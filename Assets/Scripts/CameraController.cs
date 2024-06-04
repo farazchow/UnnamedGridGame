34da15c8465ca353ce58fad5ec395447ad0a6f01
Faraz Chowdhury <farazcho@mit.edu>
2024-06-04 13:10:10 -0400

    camera updates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public static CameraController instance;
+
     public Transform cameraTransform;
+    public Transform followTransform;
 
     public float normalSpeed;
     public float fastSpeed;
@@ -13,22 +16,42 @@ public class CameraController : MonoBehaviour
     public float rotationAmount;
     public Vector3 zoomAmount;
 
-    public Quaternion newRotation;
-    public Vector3 newPosition;
-    public Vector3 newZoom;
+    private Quaternion newRotation;
+    private Vector3 newPosition;
+    private Vector3 newZoom;
+    private Vector3 dragStartPos;
+    private Vector3 dragCurrentPos;
+    private Vector3 rotateStartPos;
+    private Vector3 rotateCurrentPos;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         newPosition = transform.position;
         newRotation = transform.rotation;
         newZoom = cameraTransform.localPosition;
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleMovementInput();
+        if (followTransform != null)
+        {
+            transform.position = followTransform.position;
+        }
+        else
+        {
+            HandleMouseInput();
+            HandleMovementInput();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            followTransform = null;
+        }
     }
 
     void HandleMovementInput()
@@ -60,11 +83,11 @@ public class CameraController : MonoBehaviour
         }
 
         // Rotation
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.Q))
         {
             newRotation *= Quaternion.Euler(0, rotationAmount, 0);
         }
-        if (Input.GetKey(KeyCode.R))
+        if (Input.GetKey(KeyCode.E))
         {
             newRotation *= Quaternion.Euler(0, -rotationAmount, 0);
         }
@@ -82,5 +105,54 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, newPosition, movementTime * Time.deltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, movementTime * Time.deltaTime);
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, movementTime * Time.deltaTime);
+    }
+
+    void HandleMouseInput()
+    {   
+        // Zoom
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount * 10;
+        }
+
+        // Drag and Move
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPos = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPos = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPos - dragCurrentPos;   
+            }
+        }
+
+        //Rotate
+        if (Input.GetMouseButtonDown(2))
+        {
+            rotateStartPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPos = Input.mousePosition;
+            Vector3 rotateDifference = rotateStartPos - rotateCurrentPos;
+            rotateStartPos = rotateCurrentPos;
+
+            newRotation *= Quaternion.Euler(Vector3.up * (-rotateDifference.x / 5f));
+        }
     }
 }
