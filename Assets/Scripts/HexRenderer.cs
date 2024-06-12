d8115571a9229d7c6a0af989f6129200bea6a5cb
farazchow <farazchow@gmail.com>
2024-06-12 13:43:59 -0400

    basic grid layout and render

--- Assets/Scripts/HexRenderer.cs
+++ Assets/Scripts/HexRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,10 +24,13 @@ public class HexRenderer : MonoBehaviour
     private Mesh m_mesh;
     private MeshFilter m_meshFilter;
     private MeshRenderer m_meshRenderer;
-
     private List<Face> m_faces;
 
     public Material material;
+    public float innerSize;
+    public float outerSize;
+    public float height;
+    public bool isFlatTopped;
 
     private void Awake()
     {
@@ -61,7 +65,52 @@ public class HexRenderer : MonoBehaviour
 
     private void DrawFaces()
     {
+        m_faces = new List<Face>();
 
+        // Top Faces
+        for (int point = 0; point < 6; point++) 
+        {
+            m_faces.Add(CreateFace(innerSize, outerSize, height / 2f, height / 2f, point));
+        }
+
+        // Bottom Faces
+        for (int point = 0; point < 6; point++) 
+        {
+            m_faces.Add(CreateFace(innerSize, outerSize, -height / 2f, -height / 2f, point, true));
+        }
+
+        // Outer faces
+        for (int point = 0; point < 6; point++)
+        {
+            m_faces.Add(CreateFace(outerSize, outerSize, height / 2f, -height / 2f, point, true));
+        }
+        
+    }
+
+    private Face CreateFace(float innerRad, float outerRad, float heightA, float heightB, int point, bool reverse = false)
+    {
+        Vector3 pointA = GetPoint(innerRad, heightB, point);
+        Vector3 pointB = GetPoint(innerRad, heightB, (point < 5) ? point + 1 : 0);
+        Vector3 pointC = GetPoint(outerRad, heightA, (point < 5) ? point + 1 : 0);
+        Vector3 pointD = GetPoint(outerRad, heightA, point);
+
+        List<Vector3> vertices = new List<Vector3>() {pointA, pointB, pointC, pointD};
+        List<int> triangles = new List<int>() {0, 1, 2, 2, 3, 0};
+        List<Vector2> uvs = new List<Vector2>() {new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1)};
+
+        if (reverse) 
+        {
+            vertices.Reverse();
+        }
+
+        return new Face(vertices, triangles, uvs);
+    }
+
+    protected Vector3 GetPoint(float size, float height, int index)
+    {
+        float angle_deg = isFlatTopped ? 60 * index : 60 * index - 30;
+        float angle_rad = Mathf.PI / 180f * angle_deg;
+        return new Vector3(size * Mathf.Cos(angle_rad), height, size * Mathf.Sin(angle_rad));
     }
 
     private void CombineFaces()
@@ -77,16 +126,22 @@ public class HexRenderer : MonoBehaviour
             uvs.AddRange(m_faces[i].uvs);
 
             // Offset the triangles
-            int offset = (4 * i);
+            int offset = 4 * i;
             foreach (int tris in m_faces[i].triangles)
             {
                 triangles.Add(tris + offset);
             }
         }
-
+        
         m_mesh.vertices = vertices.ToArray();
         m_mesh.triangles = triangles.ToArray();
         m_mesh.uv = uvs.ToArray();
         m_mesh.RecalculateNormals();
+    }
+
+    public void SetMaterial(Material newMaterial)
+    {
+        material = newMaterial;
+        m_meshRenderer.material = newMaterial;
     }
 }
